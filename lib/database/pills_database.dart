e079309abed66ccc12785b258340a0e6de308c50
Suhaila-Ahmed <86728999+Suhaila-Ahmed@users.noreply.github.com>
2024-01-28 18:44:08 +0200

    Update pills_database.dart

--- lib/database/pills_database.dart
+++ lib/database/pills_database.dart
@@ -6,6 +6,7 @@ import 'package:sqflite/sqflite.dart';
 class PillsDatabase{
 
   setDatabase() async{
+    
     String databasePath = await getDatabasesPath();
     String path = join(databasePath,"pills_db");
     Database database = await openDatabase(path,version: 1,onCreate: (Database db,int version)async{
